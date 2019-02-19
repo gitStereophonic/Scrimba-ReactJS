6e41fe0b5baadf0f82dfae91b8bc1fef3abbcfae
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-02-19 19:54:46 +0300

    [DONE][BLOCK1] Solution for fist task

---
+++ FirstBlock/first-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <ul>
+    <li>First element</li>
+    <li>Second element</li>
+    <li>Third element</li>
+  </ul>,
+  document.getElementById("root"))
