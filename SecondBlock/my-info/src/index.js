4bd0fb37402d8ec68d91cd56616ab7bd1e93b355
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-02-20 15:03:01 +0300

    [UPD][BLOCK2] Added function that gives a new instanse of MyInfo component

--- SecondBlock/my-info/src/index.js
+++ SecondBlock/my-info/src/index.js
@@ -13,3 +13,16 @@ import ReactDOM from 'react-dom';
 import './style-green.css';
 
 ReactDOM.render(<div />, document.getElementById('root'));
+function MyInfo() {
+  return (
+    <div className="widget-sidebar">
+      <h1 className="title-s"> Raccoon Developer </h1>
+      <p>Blurb about myself. Coding...</p>
+      <ul className="list-ico">
+        <span>Alps</span>
+        <span>Chzeh</span>
+        <span>Tokio</span>
+      </ul>
+    </div>
+  )
+}
