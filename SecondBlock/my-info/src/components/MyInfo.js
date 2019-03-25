e679e4686dc4e9fe73aadd265625d1797436983a
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-03-25 18:16:22 +0300

    [REF] Refactored MyInfo project

---
+++ SecondBlock/my-info/src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MyInfo() {
+  return (
+    <div className="widget-sidebar">
+      <h1 className="title-s"> Raccoon Developer </h1>
+      <p>Blurb about myself. Coding...</p>
+      <ul className="list-ico">
+        <span>Alps</span>
+        <span>Chzeh</span>
+        <span>Tokio</span>
+      </ul>
+    </div>
+  )
+}
+
+export default MyInfo
