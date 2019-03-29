b14c82e2017f8609cb4810c6b3ec383341c524d7
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-03-29 16:32:56 +0300

    [ADD] Init lesson 16 project

---
+++ SeventhBlock/todo_app_phase2/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+      </div>
+    );
+  }
+}
+
+export default App;
