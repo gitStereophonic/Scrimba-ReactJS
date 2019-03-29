fbb572a31d0d3c48f8c8d8ab1bb7fcdd03e6e7bb
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-03-29 22:38:34 +0300

    [DONE] Completed lesson 19. Props in React app

--- NinethBlock/props_in_react/src/App.js
+++ NinethBlock/props_in_react/src/App.js
@@ -1,35 +1,47 @@
 import React from "react"
 
+import ContactCard from './components/ContactCard';
+
 function App() {
   return (
     <div className="contacts">
-      <div className="contact-card">
-        <img src="http://placekitten.com/300/200" alt="kitten" />
-        <h3>Mr. Whiskerson</h3>
-        <p>Phone: (212) 555-1234</p>
-        <p>Email: mr.whiskaz@catnap.meow</p>
-      </div>
 
-      <div className="contact-card">
-        <img src="http://placekitten.com/400/200" alt="kitten" />
-        <h3>Fluffykins</h3>
-        <p>Phone: (212) 555-2345</p>
-        <p>Email: fluff@me.com</p>
-      </div>
-
-      <div className="contact-card">
-        <img src="http://placekitten.com/400/300" alt="kitten" />
-        <h3>Destroyer</h3>
-        <p>Phone: (212) 555-3456</p>
-        <p>Email: ofworlds@yahoo.com</p>
-      </div>
-
-      <div className="contact-card">
-        <img src="http://placekitten.com/200/100" alt="kitten" />
-        <h3>Felix</h3>
-        <p>Phone: (212) 555-4567</p>
-        <p>Email: thecat@hotmail.com</p>
-      </div>
+      <ContactCard
+        contact={{
+          name: 'Mr. Whiskerson',
+          imgUrl: 'http://placekitten.com/300/200',
+          altTag: 'kitten',
+          phone: '(212) 555-1234',
+          email: 'mr.whiskaz@catnap.meow'
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: 'Fluffykins',
+          imgUrl: 'http://placekitten.com/400/200',
+          altTag: 'kitten',
+          phone: '(212) 555-2345',
+          email: 'fluff@me.com'
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: 'Destroyer',
+          imgUrl: 'http://placekitten.com/400/300',
+          altTag: 'kitten',
+          phone: '(212) 555-3456',
+          email: 'ofworlds@yahoo.com'
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: 'Felix',
+          imgUrl: 'http://placekitten.com/200/100',
+          altTag: 'kitten',
+          phone: '(212) 555-4567',
+          email: 'thecat@hotmail.com'
+        }}
+      />
     </div>
   )
 }
