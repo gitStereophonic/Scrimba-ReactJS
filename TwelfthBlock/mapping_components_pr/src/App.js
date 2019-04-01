c6da1ef1b03bb816aa4670565879163153de5571
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-04-01 16:31:16 +0300

    [ADD] Init 22 lesson practice project

---
+++ TwelfthBlock/mapping_components_pr/src/App.js
@@ -0,0 +1,25 @@
+/*
+ * @author Raccoon Developer
+ * @date 04.01.2019 
+ *
+ * Given a list of products (as an array of objects, as seen in productsData.js)
+ * render a <Product /> component (which you'll also need to create) for each
+ * product in the list.
+ *
+ * Make sure to use the array's `.map()` method to create these components, and 
+ * don't forget to pass a `key` prop to it to avoid the warning.
+ */
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
