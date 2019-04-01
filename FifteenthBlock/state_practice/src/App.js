1825a0a8b3ad6097f6fde318a3fc61e3e6f91073
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-04-02 02:23:04 +0300

    [ADD] Init practicum lesson 27 project

---
+++ FifteenthBlock/state_practice/src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+/*
+ * @author Raccoon Developer
+ * @date 03.29.2019
+ * 
+ * Challenge:
+ * Given an incomplete class-based component without a constructor, 
+ * add a constructor and initialize state to fix the broken component.
+ */
+
+class App extends Component() {
+  return(
+        <div>
+  <h1>{this.state.name}</h1>
+  <h3>{this.state.age} years old</h3>
+        </div >
+    )    
+}
+
+export default App
