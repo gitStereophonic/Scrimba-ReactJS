35b39ae8762f04c62c5d242002880ad23127f94b
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-04-03 00:30:02 +0300

    [ADD] Init lesson 37 project

---
+++ TwentyFirstBlock/conditional_rendering_part2/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Conditional from "./Conditional";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      unreadMessages: [
+        "Call your mom!",
+        "New spam email available. All links are definitely safe to click."
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
