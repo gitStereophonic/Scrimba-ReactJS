fdd73a30acaf61476b4d37955a9a0c649141dd04
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-04-02 17:27:40 +0300

    [ADD] Init lesspn 30 project

---
+++ SixteenthBlock/handling_events/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+  return (
+    <div>
+      <img alt='pic' src="https://www.fillmurray.com/200/100" />
+      <br />
+      <br />
+      <button onClick="myFunction()">Click me</button>
+    </div>
+  )
+}
+
+export default App
