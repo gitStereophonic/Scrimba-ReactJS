087474316ba1e02479bbf26c8c871066b3ee403a
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-04-02 20:37:33 +0300

    [DONE] Completed lesson 35

--- NineteenthBlock/lifecycle_methods1/src/App.js
+++ NineteenthBlock/lifecycle_methods1/src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 
+// https://engineering.musefind.com/react-lifecycle-methods-how-and-when-to-use-them-2111a1b692b1
+// https://reactjs.org/blog/2018/03/29/react-v-16-3.html#component-lifecycle-changes
+
 class App extends Component {
   constructor() {
     super();
     this.state = {};
   }
+
+  // will be new soon
+  static getDerivedStateFromProps(props, state) {
+    // return the new, updated state based upon the props
+    // https://reactjs.org/docs/react-component.html#static-getderivedstatefromprops
+    // https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
+  }
+
+  // will be new soon
+  getSnapshotBeforeUpdate() {
+    // create a backup of the current way things are
+    // https://reactjs.org/docs/react-component.html#getsnapshotbeforeupdate
+  }
+
+  // deprecated
+  // componentWillMount() {
+  // }
 
   componentDidMount() {
     // GET the data I need to correctly display
@@ -24,6 +44,10 @@ class App extends Component {
     // return true if want it to update
     // return false if not
   }
+
+  // deprecated
+  // componentWillUpdate() {
+  // }
 
   componentWillUnmount() {
     // teardown or cleanup your code before component disappears
