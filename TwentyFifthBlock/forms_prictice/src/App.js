e98e9f83b93a914fa53c491c41d1524602e2f1fa
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-04-04 18:53:17 +0300

    [DONE] Completed Form practice

--- TwentyFifthBlock/forms_prictice/src/App.js
+++ TwentyFifthBlock/forms_prictice/src/App.js
@@ -21,37 +21,148 @@ import React, { Component } from "react";
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      firstName: "",
+      lastName: "",
+      age: "",
+      gender: "",
+      destination: "",
+      isVegan: false,
+      isKosher: false,
+      isLactoseFree: false
+    };
+
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+
+  onChangeHandler(event) {
+    const { name, value, type, checked } = event.target;
+    type === "checkbox" ?
+      this.setState({
+        [name]: checked
+      }) :
+      this.setState({
+        [name]: value
+      });
   }
 
   render() {
     return (
       <main>
         <form>
-          <input placeholder="First Name" /><br />
-          <input placeholder="Last Name" /><br />
-          <input placeholder="Age" /><br />
+          <label>First name: </label>
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            placeholder="First Name"
+            onChange={this.onChangeHandler}
+          />
+          <br />
+          <label>Last name: </label>
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            placeholder="Last Name"
+            onChange={this.onChangeHandler}
+          />
+          <br />
+          <label>Age: </label>
+          <input
+            type="text"
+            name="age"
+            value={this.state.age}
+            placeholder="Age"
+            onChange={this.onChangeHandler}
+          />
+          <br />
 
           {/* Create radio buttons for gender here */}
+          <label>Gender: </label>
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={this.state.gender === "male"}
+              onChange={this.onChangeHandler}
+            />
+            Male
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              checked={this.state.gender === "female"}
+              onChange={this.onChangeHandler}
+            />
+            Female
+          </label>
           <br />
 
           {/* Create select box for location here */}
+          <label>Location: </label>
+          <select
+            value={this.state.destination}
+            name="destination"
+            onChange={this.onChangeHandler}
+          >
+            <option value="">-- Please Choose a destination --</option>
+            <option value="Berlin">Berlin</option>
+            <option value="London">London</option>
+            <option value="Moscow">Moscow</option>
+            <option value="Rome">Rome</option>
+            <option value="Washington">Washington</option>
+            <option value="Madrid">Madrid</option>
+          </select>
           <br />
 
           {/* Create check boxes for dietary restrictions here */}
+          <label>Dietary restrictions: </label>
+          <label>
+            <input
+              type="checkbox"
+              name="isVegan"
+              checked={this.state.isVegan}
+              onChange={this.onChangeHandler}
+            />
+            Vegan
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="isKosher"
+              checked={this.state.isKosher}
+              onChange={this.onChangeHandler}
+            />
+            Kosher
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="isLactoseFree"
+              checked={this.state.isLactoseFree}
+              onChange={this.onChangeHandler}
+            />
+            Lactose Free
+          </label>
           <br />
 
           <button>Submit</button>
         </form>
         <hr />
         <h2>Entered information:</h2>
-        <p>Your name: {/* First and last name here */}</p>
-        <p>Your age: {/* Age here */}</p>
-        <p>Your gender: {/* Gender here */}</p>
-        <p>Your destination: {/* Destination here */}</p>
+        <p>Your name: {this.state.firstName + " " + this.state.lastName}</p>
+        <p>Your age: {this.state.age}</p>
+        <p>Your gender: {this.state.gender}</p>
+        <p>Your destination: {this.state.destination}</p>
         <p>
           Your dietary restrictions:
-                    {/* Dietary restrictions here, comma separated */}
+          {this.state.isVegan ? " Vegan" : ""}
+          {this.state.isKosher ? " Kosher" : ""}
+          {this.state.isLactoseFree ? " Lactose Free" : ""}
         </p>
       </main>
     );
